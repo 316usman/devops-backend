9101ad59fc0d4e944bbfc4fbbfe69e7b142bc01a
usman <usmansiddiqui316@gmail.com>
2024-06-07 04:17:04 +0500

    debug cd

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:{DB_PASS
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-
 db = SQLAlchemy(app)
 
 class Data(db.Model):
