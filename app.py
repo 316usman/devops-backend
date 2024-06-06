4efa5c35a49ae9a5f7c095f7eef88fc544d6a849
usman <usmansiddiqui316@gmail.com>
2024-06-07 01:07:01 +0500

    debug

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     input1 = db.Column(db.String(100), nullable=False)
