04ffa4b57014b7b8167ad6bee631f6839f8d3aab
usman <usmansiddiqui316@gmail.com>
2024-06-08 20:43:32 +0500

    debug

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-CORS(app, origins=['http://10.0.4.101', 'http://10.0.5.211'])
+
+#CORS(app, origins=['http://10.0.4.101', 'http://10.0.5.211'])
+CORS(app, resources={r"/*": {"origins": "http://10.0.4.101"}})
 
 # Load environment variables from .env file
 load_dotenv()
