d17190fa0c44af4a39bbc960cca1b0127058b95e
usman <usmansiddiqui316@gmail.com>
2024-06-08 20:34:12 +0500

    cors ips

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-CORS(app, origins=['*'])
+CORS(app, origins=['http://10.0.4.101', 'http://10.0.5.211'])
 
 # Load environment variables from .env file
 load_dotenv()
